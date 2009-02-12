a9318e48dcca119f68e916aadc44908f9532330a
Robert Gerlach <https://sourceforge.net/u/khnz>
2009-02-12 04:21:00 +0000

    patch/col-diff-apply.sh: IPOS/OPOS, read -r

--- src/patch/col-diff-apply.sh
+++ src/patch/col-diff-apply.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 main() {
-  local TMP FILE HASH IOFS OOFS KEEP SKIP INSERT GOTHASH
+  local TMP FILE HASH IPOS OPOS KEEP SKIP INSERT GOTHASH
   readonly TMP="`tempfile -p cold -s .col-diff.sh.txt.tmp 2>/dev/null || echo /tmp/$(date +%s).$$.col-diff.sh.txt.tmp`"
   while
-    read FILE
-    read HASH
+    read -r FILE
+    read -r HASH
     [ -f "$FILE" ] && [ -n "$HASH" ]
   do
-    IOFS=0
-    OOFS=0
+    IPOS=0
+    OPOS=0
     rm "$TMP" 2>/dev/null
     while
-      read KEEP
-      read SKIP
-      read INSERT
+      read -r KEEP
+      read -r SKIP
+      read -r INSERT
       [ "0$KEEP" -ne 0 ] || [ "0$SKIP" -ne 0 ] || [ "0$INSERT" -ne 0 ]
     do
-      dd bs=1 if="$FILE" skip="$IOFS" of="$TMP" seek="$OOFS" count="$KEEP" 2>/dev/null
-      IOFS=$((IOFS+KEEP+SKIP))
-      OOFS=$((OOFS+KEEP))
+      dd bs=1 if="$FILE" skip="$IPOS" of="$TMP" seek="$OPOS" count="$KEEP" 2>/dev/null
+      IPOS=$((IPOS+KEEP+SKIP))
+      OPOS=$((OPOS+KEEP))
 
-      dd bs=1 of="$TMP" seek="$OOFS" count="$INSERT" 2>/dev/null
-      OOFS=$((OOFS+INSERT))
+      dd bs=1 of="$TMP" seek="$OPOS" count="$INSERT" 2>/dev/null
+      OPOS=$((OPOS+INSERT))
     done
 
     GOTHASH="`md5sum < "$TMP" | cut -d ' ' -f 1`"
